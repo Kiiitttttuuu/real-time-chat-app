5d9a521b430cc58957028d9101d33a3217da7152
Ankit Patel <kittujipatel@gmail.com>
2024-08-11 21:34:00 +0530

    Initialize project using Create React App

---
+++ server.js
@@ -0,0 +1,12 @@
+const WebSocket = require('Websocket').server;
+const http = require('http');
+
+const server = http.createServer((request,response) => {
+
+});
+
+WenSocketServer.on('request',request) => {
+    const connection = request.accept(null, request.origin);
+
+    connection.on()
+}
